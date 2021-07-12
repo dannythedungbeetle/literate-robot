b16a2b554fb98cc1a664e5a8de8cb005cf37d09b
Daniel <dannythedungbeetle@gmail.com>
2021-07-12 13:35:02 +0530

    SINGLE_TABLE inheritance example

---
+++ src/main/java/com/dan/data/springdataexample/entities/Check.java
@@ -0,0 +1,18 @@
+package com.dan.data.springdataexample.entities;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("ch")
+public class Check extends Payment {
+    private String checknumber;
+
+    public String getChecknumber() {
+        return checknumber;
+    }
+
+    public void setChecknumber(String checknumber) {
+        this.checknumber = checknumber;
+    }
+}
---
+++ src/main/java/com/dan/data/springdataexample/entities/CreditCard.java
@@ -0,0 +1,18 @@
+package com.dan.data.springdataexample.entities;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("cc")
+public class CreditCard extends Payment {
+    private String cardnumber;
+
+    public String getCardnumber() {
+        return cardnumber;
+    }
+
+    public void setCardnumber(String cardnumber) {
+        this.cardnumber = cardnumber;
+    }
+}
